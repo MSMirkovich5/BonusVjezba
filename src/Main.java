53928cf467eddb93cc1c55775cbd678404088447
BottleUser222 <marko.s.mirkovic@gmail.com>
2026-02-17 18:57:08 +0100

    Zadatak 5

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,7 @@ public class Main {
          */
 
         //Zadatak 4 - Polja Brojeva:
+        /*
         Scanner input = new Scanner(System.in);
         System.out.print("Koliko brojeva želite u polju? ");
         int koliko = input.nextInt();
@@ -56,6 +57,36 @@ public class Main {
             poljeBrojeva[i] = input.nextInt();
         }
         System.out.println("Najveći broj je: " + Arrays.stream(poljeBrojeva).max().getAsInt() + " Najmanji broj je: " + Arrays.stream(poljeBrojeva).min().getAsInt());
+
+         */
+
+        //Zadatak 5 - OIB:
+        Scanner input = new Scanner(System.in);
+        System.out.print("Unesite OIB: ");
+        String oib = input.nextLine();
+        if (oib.length() != 11) {
+            System.out.print("OIB mora biti 11 znamenki!");
+        }
+        else {
+            int product = 10;
+            for (int i = 0; i < 10; i++) {
+                int znamenka = Character.getNumericValue(oib.charAt(i));
+                int sum = (znamenka + product) % 10;
+                if (sum == 0) {
+                    sum = 10;
+                }
+                product = (sum * 2) % 11;
+            }
+            int provjeraZnam = 11 - product;
+            if (provjeraZnam == 10) {
+                provjeraZnam = 0;
+            }
+            int zadnjaZnam = Character.getNumericValue(oib.charAt(10));
+            if (provjeraZnam == zadnjaZnam) {
+                System.out.println("OIB je ispravan!");
+            }
+            else System.out.println("OIB ne valja!");
+        }
     }
 
 }
